364ecbf204a3f4879bf38d88015f89c05dc61eaa
danylokotur <kotur.danylo@gmail.com>
2023-12-11 14:34:57 +0200

    edc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {UsersComponent} from "./components/users/users.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    imports: [CommonModule, RouterOutlet, UsersComponent],
+  template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
   title = 'angular';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import {MainLayoutComponent} from "./layouts/main-layout/main-layout.component";
+import {PostsPageComponent, UserDetailsPageComponent, UsersPageComponent} from "./pages";
+
+export const routes: Routes = [
+    {
+        path: '', component: MainLayoutComponent, children: [
+            {path: '', redirectTo: 'users', pathMatch: 'full'},
+            {
+                path: 'users', component: UsersPageComponent, children: [
+                    {path: ':id', component: UserDetailsPageComponent}
+                ]
+            },
+            {path: 'posts', component: PostsPageComponent}
+        ]
+    }
+];
