77b21eace350f3f639569dc499ff2e67e39527ef
danylokotur <kotur.danylo@gmail.com>
2023-12-12 20:22:19 +0200

    edc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {CarFormComponent} from "./components/car-form/car-form.component";
+import {CarsComponent} from "./components/cars/cars.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CarFormComponent, CarsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'untitled3';
 }
